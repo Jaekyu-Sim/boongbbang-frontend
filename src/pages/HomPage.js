9e0ab44840dfb3dfa70f0606afd581c36a4cff54
Jaekyu Sim <worb1605@naver.com>
2022-11-30 00:54:13 +0900

    add naver map api

--- src/pages/HomPage.js
+++ src/pages/HomPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import NaverApiMap from "../utils/NaverAPIMap";
+
 const HomePage = () => {
 
     const navigate = useNavigate();
@@ -19,6 +21,7 @@ const HomePage = () => {
             <button onClick={navigateToTestpage2}>
                 To Page 2
             </button>
+            <NaverApiMap></NaverApiMap>
         </div>
     )
 }
